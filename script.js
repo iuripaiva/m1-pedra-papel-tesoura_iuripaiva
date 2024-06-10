f060870d20b9491fa391978e59b4a488ee8f71a5
Iuri Paiva <iuripsilva27@gmail.com>
2024-06-09 22:33:53 -0300

    Adicionada a lógica do pedra papel tesoura

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 /* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(player1, player2){
+
+    if (player1 == player2) { // Lógica do empate
+        return "Empate!";
+    } else if ( // Lógica da vitória do Jogador 1
+        (player1 == "Pedra" && player2 == "Tesoura") ||
+        (player1 == "Papel" && player2 == "Pedra") ||
+        (player1 == "Tesoura" && player2 == "Papel")
+    ) {
+        return "Jogador 1 venceu!";
+    } else if ( // Lógica da vitória do Jogador 2
+        (player2 == "Pedra" && player1 == "Tesoura") ||
+        (player2 == "Papel" && player1 == "Pedra") ||
+        (player2 == "Tesoura" && player1 == "Papel")
+    ) {
+        return "Jogador 2 venceu!";
+    } else { // Caso seja atribuído um valor inválido
+        return "Valor inválido!";
+    }
+
+}
