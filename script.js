c423e0b006e339f17cee31fe88460165ffed4e82
Iuri Paiva <47199107+iuripaiva@users.noreply.github.com>
2024-06-10 12:08:04 -0300

    Removidos os comentários desnecessários

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-/* Desenvolva seu código abaixo */
-
 function playRockPaperScissor(player1, player2){
 
-    if (player1 == player2) { // Lógica do empate
+    if (player1 == player2) {
         return "Empate!";
-    } else if ( // Lógica da vitória do Jogador 1
+    } else if (
         (player1 == "Pedra" && player2 == "Tesoura") ||
         (player1 == "Papel" && player2 == "Pedra") ||
         (player1 == "Tesoura" && player2 == "Papel")
     ) {
         return "Jogador 1 venceu!";
-    } else if ( // Lógica da vitória do Jogador 2
+    } else if (
         (player2 == "Pedra" && player1 == "Tesoura") ||
         (player2 == "Papel" && player1 == "Pedra") ||
         (player2 == "Tesoura" && player1 == "Papel")
     ) {
         return "Jogador 2 venceu!";
-    } else { // Caso seja atribuído um valor inválido
+    } else {
         return "Valor inválido!";
     }
 
